967c176c7c1ae5bc46a914c6bdedfc084a0f787c
chirag1992m <chirag1992m.cc@gmail.com>
2016-12-18 02:16:15 -0500

    Adding the benchmark model.

---
+++ models/benchmark.py
@@ -0,0 +1,27 @@
+#A benchmark model
+
+#Simply copies takes the last quote/trade bid-ask value
+#And predicts them for the next 50 time periods
+
+import numpy as np
+import pandas as pd
+
+prediction = pd.DataFrame.from_csv('../predictions/template_prediction.csv')
+
+for ix, row in prediction.iterrows():
+	row_id = row['row_id']
+
+	test_row = test_set[test_set['row_id'] == row_id]
+	#Get the ask50 and bid50 in the test set
+	ask = test_row['ask50'].values[0]
+	bid = test_row['bid50'].values[0]
+
+	#And copy them in the full row
+	for column in prediction.columns.values:
+		if column != 'row_id':
+			if column.startswith('ask'):
+				prediction.set_value(ix, column, ask)
+			else:
+				prediction.set_value(ix, column, bid)
+
+prediction.to_csv('../predictions/benchmark.csv')
