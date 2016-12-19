c0f7855e47b417bab91b874bfbd597887eb79960
chirag1992m <chirag1992m.cc@gmail.com>
2016-12-18 23:45:53 -0500

    Model: Random-Forest with Separated buyer-seller initiated data

---
+++ models/RF_SB.py
@@ -0,0 +1,183 @@
+#A simple Random Forest Regressor model which analzes seller 
+#and buyer initiated transactions separately.
+
+#Generates the first ask and bid which is copied
+#across the all the upcoming bid and ask
+#as prediction
+import pandas as pd
+import numpy as np
+from sklearn import ensemble
+
+import pickle
+
+#Read the data
+print "Loading data..."
+train_table = pd.DataFrame.from_csv('../data/subset_train_OHE.csv')
+test_table = pd.DataFrame.from_csv('../data/subset_test_OHE.csv')
+
+predictionColumns = []
+for i in range(52, 101):
+	for column in train_table.columns.values:
+		if column.endswith(str(i)) and (column.startswith('ask') or column.startswith('bid')):
+			predictionColumns.append(column)
+
+featureColumns = []
+columnsToIgnore = ['row_id', 'is_seller', 'is_buyer']
+for column in train_table.columns.values:
+	if ((column not in predictionColumns) and (column not in columnsToIgnore) and (not column.startswith('time'))):
+		featureColumns.append(column)
+
+train_table_seller = train_table[train_table['is_seller'] == 1]
+train_table_buyer = train_table[train_table['is_buyer'] == 1]
+
+test_table_seller = test_table[test_table['is_seller'] == 1]
+test_table_buyer = test_table[test_table['is_buyer'] == 1]
+
+trainX_seller = np.zeros((train_table_seller.shape[0] * 2, len(featureColumns) + 1))
+trainY_seller = np.zeros((train_table_seller.shape[0] * 2))
+trainX_buyer = np.zeros((train_table_buyer.shape[0] * 2, len(featureColumns) + 1))
+trainY_buyer = np.zeros((train_table_buyer.shape[0] * 2))
+
+testX_seller = np.zeros((test_table_seller.shape[0] * 2, len(featureColumns) + 1))
+testX_buyer = np.zeros((test_table_buyer.shape[0] * 2, len(featureColumns) + 1))
+
+print "Creating Predictors..."
+index = 0
+for ix, row in train_table_seller.iterrows():
+	X_features = (np.array(row[featureColumns])).flatten('F')
+	X = np.concatenate((X_features, np.array([0])))
+	Y = row[predictionColumns[0]]
+	trainX_seller[index, :] = X
+	trainY_seller[index] = Y
+
+	index = index+1
+
+	X = np.concatenate((X_features, np.array([1])))
+	Y = row[predictionColumns[1]]
+	trainX_seller[index, :] = X
+	trainY_seller[index] = Y
+	index = index+1
+
+index = 0
+for ix, row in train_table_buyer.iterrows():
+	X_features = (np.array(row[featureColumns])).flatten('F')
+	X = np.concatenate((X_features, np.array([0])))
+	Y = row[predictionColumns[0]]
+	trainX_buyer[index, :] = X
+	trainY_buyer[index] = Y
+
+	index = index+1
+
+	X = np.concatenate((X_features, np.array([1])))
+	Y = row[predictionColumns[1]]
+	trainX_buyer[index, :] = X
+	trainY_buyer[index] = Y
+	index = index+1
+
+index = 0
+for ix, row in test_table_seller.iterrows():
+	X_features = (np.array(row[featureColumns])).flatten('F')
+	X = np.concatenate((X_features, np.array([0])))
+	testX_seller[index, :] = X
+
+	index = index+1
+
+	X = np.concatenate((X_features, np.array([1])))
+	testX_seller[index, :] = X
+	index = index+1
+
+index = 0
+for ix, row in test_table_buyer.iterrows():
+	X_features = (np.array(row[featureColumns])).flatten('F')
+	X = np.concatenate((X_features, np.array([0])))
+	testX_buyer[index, :] = X
+
+	index = index+1
+
+	X = np.concatenate((X_features, np.array([1])))
+	testX_buyer[index, :] = X
+	index = index+1
+
+print "Training..."
+RF_model_seller = ensemble.RandomForestRegressor(n_estimators=200,
+	criterion='mse',
+	max_depth=None,
+	min_samples_split=2, 
+	min_samples_leaf=1, 
+	min_weight_fraction_leaf=0.0, 
+	max_features='auto', 
+	max_leaf_nodes=None, 
+	min_impurity_split=1e-07, 
+	bootstrap=True, 
+	oob_score=False, 
+	n_jobs=-1, 
+	random_state=None, 
+	verbose=1, 
+	warm_start=False)
+RF_model_seller.fit(trainX_seller, trainY_seller)
+
+RF_model_buyer = ensemble.RandomForestRegressor(n_estimators=200,
+	criterion='mse',
+	max_depth=None,
+	min_samples_split=2, 
+	min_samples_leaf=1, 
+	min_weight_fraction_leaf=0.0, 
+	max_features='auto', 
+	max_leaf_nodes=None, 
+	min_impurity_split=1e-07, 
+	bootstrap=True, 
+	oob_score=False, 
+	n_jobs=-1, 
+	random_state=None, 
+	verbose=1, 
+	warm_start=False)
+RF_model_buyer.fit(trainX_buyer, trainY_buyer)
+
+models = {'seller': RF_model_seller, 'buyer': RF_model_buyer}
+
+with open('../run_models/RF_SB.model', 'wb') as output:
+	pickle.dump(models, output, -1)
+
+print "Predicting..."
+testY_seller = RF_model_seller.predict(testX_seller)
+testY_buyer = RF_model_buyer.predict(testX_buyer)
+
+prediction = pd.DataFrame.from_csv('../predictions/template_prediction.csv')
+
+i = 0
+for ix, row in test_table_seller.iterrows():
+	row_id = row['row_id']
+
+	index_in_pred = prediction[prediction['row_id'] == row_id].index.tolist()[0]
+
+	bid = testY_seller[i]
+	i = i+1
+	ask = testY_seller[i]
+	i = i+1
+
+	for column in predictionColumns:
+		if column.startswith('bid'):
+			prediction.set_value(index_in_pred, column, bid)
+		else:
+			prediction.set_value(index_in_pred, column, ask)
+
+i = 0
+for ix, row in test_table_buyer.iterrows():
+	row_id = row['row_id']
+
+	index_in_pred = prediction[prediction['row_id'] == row_id].index.tolist()[0]
+
+	bid = testY_buyer[i]
+	i = i+1
+	ask = testY_buyer[i]
+	i = i+1
+
+	for column in predictionColumns:
+		if column.startswith('bid'):
+			prediction.set_value(index_in_pred, column, bid)
+		else:
+			prediction.set_value(index_in_pred, column, ask)
+
+prediction.to_csv('../predictions/RF_SB.csv')
+
+print "Done!"
